8ac4f33dccc778e9ab7fb33f536bde196e1a74c2
Asa Choi <asa.choi@gmail.com>
2023-04-27 15:33:56 +0000

    Update 6 files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import "./styles.css";
+import { ReactMic } from "react-mic";
+import { useState } from "react";
+const { Configuration, OpenAIApi } = require("openai");
 
 export default function App() {
+  const [record, setRecord] = useState(false);
+  const configuration = new Configuration({
+    apiKey: process.env.OPENAI_KEY,
+  });
+
+  function onStop(recordedBlob) {
+    console.log("recordedBlob is: ", recordedBlob);
+  }
+
+  const switchRecord = () => {
+    setRecord(!record);
+  };
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      {process.env.OPENAI_KEY}
+      <ReactMic record={record} onStop={onStop} mimeType="audio/mp3" />
+      <br />
+      <button onClick={switchRecord} type="button">
+        {record ? "Stop" : "Start"}
+      </button>
     </div>
   );
 }
